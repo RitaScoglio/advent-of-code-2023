380eda944ffebf4f68e85c386930906f5de7f686
Rita Scoglio <54063396+RitaScoglio@users.noreply.github.com>
2023-12-01 14:56:57 +0100

    Day01

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -5,6 +5,8 @@ class Day01 {
 
     val NUMBERS = ('1'..'9').toList()
 
+    val WORD_NUMBERS = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
     fun readFileAsList(fileName: String): List<String> = File(fileName).useLines { it.toList() }
 
     fun main() {
@@ -14,24 +16,22 @@ class Day01 {
     }
 
     fun firstStar(input: List<String>): Int {
-        return input.map { string -> string.filter { it in NUMBERS } }
-            .map { it.first().toString() + it.last() }
-            .map { it.toInt() }
+        return input.map { line -> line.filter { it in NUMBERS }.let { (it.first().toString() + it.last()).toInt() } }
             .sum()
     }
 
-    fun secondStar(input: List<String>): Int {
-        val numbers = input.map { s ->
-            s.replace("one", "one1one")
-                .replace("two", "two2two")
-                .replace("three", "three3three")
-                .replace("four", "four4four")
-                .replace("five", "five5five")
-                .replace("six", "six6six")
-                .replace("seven", "seven7seven")
-                .replace("eight", "eight8eight")
-                .replace("nine", "nine9nine")
+    fun secondStar(input: List<String>): Int = input.map { line ->
+        replaceWordWithNumber(line, String::findAnyOf)?.let {
+            replaceWordWithNumber(
+                it, String::findLastAnyOf
+            )
         }
-        return firstStar(numbers)
-    }
+    }.let { firstStar(it as List<String>) }
+
+    fun replaceWordWithNumber(line: String, function: (String, Collection<String>) -> Pair<Int, String>?): String? =
+        function(line, WORD_NUMBERS)
+            ?.let { (index, number) ->
+                line.take(index) + (WORD_NUMBERS.indexOf(number) + 1) + line.drop(index)
+            }
 }
+
